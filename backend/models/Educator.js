441adf4b04498b2c6bcdfe419d968a67cc67507e
unknown <talaviyakruti11@gmail.com>
2025-01-07 10:03:09 +0530

    Your descriptive commit message

--- backend/models/Educator.js
+++ backend/models/Educator.js
@@ -1,15 +1,10 @@
-// models/Educator.js
 const mongoose = require('mongoose');
 
 const educatorSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  degree: { type: String },
+  educator_id: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId(), unique: true }, // Primary key
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
   photo: { type: String },
-  subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Farmer' }],
-  uploaded_videos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Video' }],
-  articles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
-  cheat_sheets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CheatSheet' }],
-}, { timestamps: true });
+  eduAccCreatedDate: { type: Date, default: Date.now },
+}, { timestamps: true }); 
 
 module.exports = mongoose.model('Educator', educatorSchema);
