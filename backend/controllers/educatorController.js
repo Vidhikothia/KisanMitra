bf577b3b6acbf3aabaa04740ffc6b0548910f9a3
unknown <talaviyakruti11@gmail.com>
2025-02-23 15:57:21 +0530

    by kruti

--- backend/controllers/educatorController.js
+++ backend/controllers/educatorController.js
@@ -1,11 +1,15 @@
 const Educator = require('../models/Educator');
 const cloudinary = require('cloudinary').v2;
+const mongoose = require('mongoose');
+const User = require('../models/User'); // ✅ Import User model
+
 
 // Create a new educator
 exports.createEducator = async (req, res) => {
     try {
         let photoUrl = null;
-
+        const userId = req.user._id;
+        console.log("Extracted userId from JWT:", userId);
         // Handle photo upload if a file is provided
         if (req.file) {
             const result = await cloudinary.uploader.upload(req.file.path, {
@@ -18,13 +22,17 @@ exports.createEducator = async (req, res) => {
 
         // Create the educator object using authenticated user's ID
         const newEducator = new Educator({
-            user_id: req.user._id, // Use the ID from the authenticated user
+            user_id: new mongoose.Types.ObjectId(userId), // Use the ID from the authenticated user
             photo: photoUrl
         });
 
         // Save the educator to the database
         const savedEducator = await newEducator.save();
-
+        const user = await User.findById(userId);
+        if (user.role !== 'Educator') {
+            user.role = 'Educator';
+            await user.save();
+        }
         res.status(201).json({
             message: 'Educator created successfully!',
             educator: savedEducator
